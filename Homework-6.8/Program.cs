c374b4cab14b2a77b8ebbafa725461652156ed97
Dju <julian.fjodorov@gmail.com>
2023-02-28 21:14:15 +0200

    Exercise 6.8 & 6.9
    6.8 done
    6.9 in progress

--- Homework-6.8/Program.cs
+++ Homework-6.8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 /* Напишите программу с классом, в котором есть два поля: символьное и текстовое.
  * В классе должен быть перегруженный метод для присваивания значений полям.
  * Если метод вызывается с символьным аргументов, то соответствующее значение присваивается символьному полю.
@@ -8,3 +9,59 @@
  * В противном случае (если в массиве больше одного элемента) из символов массива формируется текстовая строка и 
  * присваивается значением текстовому полю.
  */
+
+class MyClass
+{
+    char symb;
+    string? text;
+    public char GetInfo(char k)
+    {
+        this.symb = k;
+        return symb;
+    }
+    public string GetInfo(string k)
+    {
+        this.text = k;
+        return text;
+    }
+
+    public void GetInfo(char[] Array)
+    {
+        int lenght = Array.Length;
+        if (lenght == 1)
+        {
+            symb = Array[0];
+        }
+        else
+        {
+            text = new string (Array);
+        }
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("Char: " + symb + " & string: " + text);
+    }
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        MyClass A = new MyClass();
+        A.Show();
+        A.GetInfo('F');
+        A.Show();
+        A.GetInfo("hhhh");
+        A.Show();
+        char[] example1 = { 'K' };
+        A.GetInfo(example1);
+        A.Show();
+        char[] example2 = { 'L', 'O','X' };
+        A.GetInfo('O');
+        A.GetInfo(example2);
+        A.Show();
+        MyClass B = new MyClass();
+        B.Show();
+    }
+}
---
+++ Homework-6.9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+/* Напишите программу с классом, в котором есть статические методы, 
+ * которым можно передавать произвольное количество целочисленных аргументов (или целочисленный массив).
+ * Методы, на основании переданных аргументов или массива, позволяют вычислить - наибольшее значение,
+ * а так же среднее значение из набора чисел. */
+
+class MyClass
+{
+    static int max;
+    static int middle;
+
+    public static void GetNumber(params int[] num)
+    {
+        int max = num.Max();
+        int middle = (int)num.Average();
+        
+    }
+    public void Show()
+    {
+        Console.WriteLine("Max number is: " + max + " and middle number is: " + middle);
+    }
+
+    class MainClass
+    {
+        static void Main()
+        {
+            int[] example1 = { 1, 4, 6, 8, 9, 0, 6 };
+            MyClass Demo1 = new MyClass();
+            GetNumber(5,6);
+            Demo1.Show();
+            GetNumber(example1);
+            Demo1.Show();
+        }
+    }
+}

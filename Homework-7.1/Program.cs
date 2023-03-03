d540b75dc134bda8d0863640106a7337e4407740
Dju <julian.fjodorov@gmail.com>
2023-03-03 14:30:59 +0200

    7.1 & 7.2
    Exercise 7.1 done
    7.2 started
    1 h spent

--- Homework-7.1/Program.cs
+++ Homework-7.1/Program.cs
@@ -1,55 +1,58 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Text;
 /* Напишите программу, в которой есть статический метод. Аргументом методу передаётся текстовое значение.
  * Результатом метод возвращает текст, в котором, по сравнению с текстом аргументом,
  * между символами вставлены пробелы. */
 
 class MyClass
 {
-    string k;
+    public string clsFeild;
+    public string clsFeildGPT;
 
     public static string SpaceChars (string k)
     {
         char[] charArr = k.ToCharArray();
         char[] copyArr = charArr;
-        Console.WriteLine(charArr.Length);
-        Console.WriteLine(charArr);
-        int p = 0;
-        for (int l = 0; l < k.Length; l++,p++)
-        {
-        }
-
-        Array.Resize(ref charArr, charArr.Length + p);
-        Console.WriteLine(charArr.Length);
-        Console.WriteLine(charArr);
-
+        int plus = charArr.Length;
+        Array.Resize(ref charArr, charArr.Length + plus);
         Array.Clear(charArr);
-        Console.WriteLine(charArr.Length);
-        Console.WriteLine(charArr);
         int j = 0;
-        char symb = ' ';
-        for (int l =0 ;l<p;l++)
+        char symb = '_';
+        for (int i =0 ;i<plus;i++)
         {
-            charArr[j] = copyArr[l];
+            charArr[j] = copyArr[i];
             j++;
             charArr[j] = symb;
             j++;
         }
-        Console.WriteLine(charArr.Length);
-        Console.WriteLine(charArr);
         string newk = new string(charArr);
 
         return newk;
     }
 
-    public void Show()
+
+    public static string SpaceCharsGPT(string input)
     {
-        Console.WriteLine("Our string is: "+k);
+        StringBuilder sb = new StringBuilder();
+        foreach (char c in input)
+        {
+            sb.Append(c);
+            sb.Append('_');
+        }
+        return sb.ToString();
     }
 
-    public MyClass(string k)
+    public void Show()
     {
-        SpaceChars(k);
+        Console.WriteLine("Our string (made by me ) is: "+clsFeild);
+        Console.WriteLine("Our string (made by GPT) is: " + clsFeild);
+    }
+
+    public MyClass(string input)
+    {
+        clsFeild = SpaceChars(input);
+        clsFeildGPT = SpaceCharsGPT(input);
     }
 }
 
@@ -59,7 +62,7 @@ class MainClass
     {
         MyClass A = new MyClass("Hello!");
         A.Show();
-        MyClass B = new MyClass("Goodbye");
+        MyClass B = new MyClass("Hello my friend, how are you?");
         B.Show();
         A.Show();
     }

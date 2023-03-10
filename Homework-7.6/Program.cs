80a71f1937d6c84aec160aaea4936d537b18e968
Dju <julian.fjodorov@gmail.com>
2023-03-10 13:30:07 +0200

    7.6 done
    7.6 done
    used:
    System.Text.RegularExpressions; (Regex)
    System.Linq; (HashSet)
    
    1h30m spent

--- Homework-7.6/Program.cs
+++ Homework-7.6/Program.cs
@@ -5,4 +5,55 @@
  */
 
 using System;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text.RegularExpressions;
 
+
+class MyClass
+{
+    public static char[] SymbArrReturn(string input)
+    {
+        input = Regex.Replace(input, "[- .?!)|/(,:;'_[0-9\\]]", "");
+        input=input.ToLower();
+        input = String.Concat(input.OrderBy(ch => ch));
+
+        char[] charArr = input.ToCharArray();
+
+        char[] newArr = removeDuplicates(charArr);
+        static T[] removeDuplicates<T>(T[] charArray)
+        {
+            HashSet<T> set = new HashSet<T>(charArray);
+            T[] result = new T[set.Count];
+            set.CopyTo(result);
+            return result;
+        }
+
+        return newArr;
+    }
+    public MyClass(string input)
+    {
+        char[]charArr = SymbArrReturn(input);
+        Show();
+
+
+        void Show()
+        {
+            Console.Write("Result:");
+            foreach (char i in charArr)
+            {
+                Console.Write(" " + i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        MyClass A = new MyClass("Baca! de; f/|g G4g");
+    }
+
+}
